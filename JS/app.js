5215726f66709b771704365f4fc261d27d1a3f94
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-26 15:40:47 +0600

    Full project completed

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,63 @@
-const cardInput = document.getElementById('card-input')
+const mainDiv = document.getElementById("card-container");
+const cardInput = document.getElementById("card-input");
+const errorMsg = document.getElementById("error-msg");
 const favCard = () => {
-    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=${cardInput.value}`
+  const cardValue = cardInput.value;
+  if (isNaN(cardValue) || cardValue < 1) {
+    mainDiv.textContent = "";
+    errorMsg.innerHTML = `😪 Please Input a valid Number not <span class="text-warning"> "${cardValue}"</span>`;
+    cardInput.value = "";
+  } else if (cardValue > 52) {
+    errorMsg.textContent = "";
+    mainDiv.textContent = "";
+    errorMsg.innerHTML = `🤐 Please Input equal or smaller number than <span class="text-success">"52"</span>`;
+    cardInput.value = "";
+  } else {
+    errorMsg.textContent = "";
+    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=${cardInput.value}`;
     fetch(url)
-    .then(response => response.json())
-    .then(json => printCard(json.cards))
-}
+      .then((response) => response.json())
+      .then((json) => printCard(json.cards));
+    cardInput.value = "";
+  }
+};
 
-const mainDiv = document.getElementById('card-container')
-const printCard = cards => {
-    cards.forEach(element => {
-        console.log(element)
-        const cardDiv = document.createElement('div')
-        cardDiv.className = 'col-4'
-        cardDiv.className = 'mb-3'
-        cardDiv.innerHTML = `
+const printCard = (cards) => {
+  cards.forEach((element) => {
+    console.log(element);
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "col-4";
+    cardDiv.className = "mb-3";
+    cardDiv.innerHTML = `
         <div class="col rounded-3 border border-3 border-dark">
             <div class="card">
                 <img src="${element.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="card-title">${element.code}</h3>
                     <p class="card-text"> ${element.suit} - ${element.value}</p>
-                    <a href="#" class="btn btn-primary">See Details</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary">See Details</button>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade text-center" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title" id="exampleModalLabel">${element.code}</h3>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <img src="${element.image}" alt="">
+                    <h5 class="mt-3">${element.suit} ~ ${element.value}</h5>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
-        `
-        mainDiv.appendChild(cardDiv)
-    });
-}
+        `;
+    mainDiv.appendChild(cardDiv);
+  });
+};
